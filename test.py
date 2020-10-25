03b1c3ad6dc0709325fa7a0012e98f320df1a0c0
kiwi0401 <kiwi04011@gmail.com>
2020-10-24 20:03:15 -0700

    cleanup

--- test.py
+++ test.py
@@ -20,6 +20,7 @@ def connectionListener(connected, info):
         notified[0] = True
         cond.notify()
 
+    
 NetworkTables.initialize(server='127.0.0.1')
 NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
 
@@ -31,63 +32,70 @@ with cond:
 shuffleboard = NetworkTables.getTable('Shuffleboard')
 tab = shuffleboard.getSubTable('Test3DGrapher')
 
-
-
 plt.ion()
 fig = plt.figure()
 ax = Axes3D(fig)
 
-i = 0
-while(i < 1):
-
-    while(NetworkTables.isConnected()):
-        vx = tab.getValue('vx', 0)
-        vy = tab.getValue('vy', 0)
-        vz = tab.getValue('vz', 0)
-        T = tab.getValue('T', 0)
-
-        t = np.linspace(0, T, 100)
-        x = vx * t
-        y = vy * t
-        z = vz * t - 9.8 * t * t
-
-        xlim = vx * T
-        ylim = vy * T
-        zlim = vz * T - 9.8 * T * T
-        ax.clear()
-        ax.plot(x, y, z,'g', label='Turret Aim')
-        
-        c = Circle((ylim,zlim),0.5)
-        s = Rectangle((ylim,zlim - 0.14),0.25,0.25,45)
-        s.set_color('r')
-        s.set_alpha(0.5)
-        c.set_color('b')
-        c.set_alpha(0.4)
-        ax.add_patch(c)
-        ax.add_patch(s)
-        art3d.pathpatch_2d_to_3d(c, z=zlim, zdir = "x")
-        art3d.pathpatch_2d_to_3d(s, z=zlim, zdir = "x")
-        ax.set_aspect('auto')
-
-        if xlim > ylim:
-            ylim = xlim
-        else:
-            xlim = ylim
-
-        ax.set_xlim3d(0, xlim)
-        ax.set_ylim3d(-ylim, ylim)
-        plt.legend()
-        plt.show()
-        plt.pause(.02)
-
-    print('lost connection')
+def connect():
     with cond:
-        print("Looking for Shuffleboard")
+        print("Looking for Shuffleboard on Network")
         if not notified[0]:
             cond.wait()
     time.sleep(4)
 
+def gatherData():
+    global x,y,z,t,xlim,ylim,zlim
+    while(True):
+        while(NetworkTables.isConnected()):
+            vx = tab.getValue('vx', 0)
+            vy = tab.getValue('vy', 0)
+            vz = tab.getValue('vz', 0)
+            T = tab.getValue('T', 0)
 
+            t = np.linspace(0, T, 100)
+            x = vx * t
+            y = vy * t
+            z = vz * t - 9.8 * t * t
 
+            xlim = vx * T
+            ylim = vy * T
+            zlim = vz * T - 9.8 * T * T
+            plotstuff()
+        connect()
+
+def plotstuff():
+    global xlim,ylim
+    ax.clear()
+    ax.plot(x, y, z,'g', label='Ball Path')
+    
+    c = Circle((ylim,zlim),0.5)
+    s = Rectangle((ylim,zlim - 0.16),0.25,0.25,45)
+    s.set_color('r')
+    s.set_alpha(0.5)
+    c.set_color('b')
+    c.set_alpha(0.4)
+    ax.add_patch(c)
+    ax.add_patch(s)
+    art3d.pathpatch_2d_to_3d(c, z=xlim, zdir = "x")
+    art3d.pathpatch_2d_to_3d(s, z=xlim, zdir = "x")
+    ax.set_aspect('auto')
+    if(ylim == 0 or xlim == 0):
+        xlim = 5
+        ax.set_zlim3d(0,5)
+
+    if xlim > ylim:
+        ylim = xlim
+    else:
+        xlim = ylim
+
+    
+
+    ax.set_xlim3d(0, xlim)
+    ax.set_ylim3d(-ylim, ylim)
+    plt.legend()
+    plt.show()
+    plt.pause(.02)
+
+gatherData()
 
 
