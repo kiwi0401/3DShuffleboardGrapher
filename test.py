98b83be5933ca08779545a01fd6cb24da0f7ee7f
kiwi0401 <kiwi04011@gmail.com>
2021-08-14 23:18:26 -0500

    robot move

--- test.py
+++ test.py
@@ -9,18 +9,21 @@ from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits import mplot3d
 from networktables import NetworkTables
 import mpl_toolkits.mplot3d.art3d as art3d
+import math
 
 sys.setrecursionlimit(5000)
 
 cond = threading.Condition()
 notified = [False]
+
+
 def connectionListener(connected, info):
     print(info, '; Connected=%s' % connected)
     with cond:
         notified[0] = True
         cond.notify()
 
-    
+
 NetworkTables.initialize(server='127.0.0.1')
 NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
 
@@ -30,11 +33,12 @@ with cond:
         cond.wait()
 
 shuffleboard = NetworkTables.getTable('Shuffleboard')
-tab = shuffleboard.getSubTable('Test3DGrapher')
+tab = shuffleboard.getSubTable('Physics')
 
 plt.ion()
 fig = plt.figure()
 ax = Axes3D(fig)
+
 
 def connect():
     with cond:
@@ -43,59 +47,73 @@ def connect():
             cond.wait()
     time.sleep(4)
 
+
 def gatherData():
-    global x,y,z,t,xlim,ylim,zlim
+    global x, y, z, t, xlim, ylim, zlim, xx, yy, zz
     while(True):
         while(NetworkTables.isConnected()):
             vx = tab.getValue('vx', 0)
             vy = tab.getValue('vy', 0)
             vz = tab.getValue('vz', 0)
+            dtvx = tab.getValue('driveTrainVX', 0)
+            dtvy = tab.getValue('driveTrainVY', 0)
+
+            fv = tab.getValue('flywheelVel', 0)
+            ta = tab.getValue('turretAngle', 0)
+            ha = tab.getValue('hoodAngle', 0)
+
+            vya = fv * np.sin(np.deg2rad(ta))
+            vxa = fv * np.cos(np.deg2rad(ta))
+
             T = tab.getValue('T', 0)
 
             t = np.linspace(0, T, 100)
-            x = vx * t
-            y = vy * t
-            z = vz * t - 9.8 * t * t
+            x = (vx + dtvx) * t
+            y = (vy + dtvy) * t
+            z = vz * t - (1 / 2) * 9.81 * t * t
+
+            xx = (vxa * np.sin(np.deg2rad(ha)) - dtvx) * t
+            yy = ((fv * np.cos(np.deg2rad(ta))) - dtvy) * t
+            zz = vya * t - (1 / 2) * 9.81 * t * t
 
             xlim = vx * T
             ylim = vy * T
-            zlim = vz * T - 9.8 * T * T
+            zlim = vz * T - (1 / 2) * 9.8 * T * T
             plotstuff()
         connect()
 
+
 def plotstuff():
-    global xlim,ylim
+    global xlim, ylim
     ax.clear()
-    ax.plot(x, y, z,'g', label='Ball Path')
-    
-    c = Circle((ylim,zlim),0.5)
-    s = Rectangle((ylim,zlim - 0.16),0.25,0.25,45)
+    ax.plot(x, y, z, 'g', label='Ball Path')
+    ax.plot(x, y, zz, 'g', label='Ball Path')
+
+    c = Circle((ylim, zlim), 0.5)
+    s = Rectangle((ylim, zlim - 0.16), 0.25, 0.25, 45)
     s.set_color('r')
     s.set_alpha(0.5)
     c.set_color('b')
     c.set_alpha(0.4)
     ax.add_patch(c)
     ax.add_patch(s)
-    art3d.pathpatch_2d_to_3d(c, z=xlim, zdir = "x")
-    art3d.pathpatch_2d_to_3d(s, z=xlim, zdir = "x")
+    art3d.pathpatch_2d_to_3d(c, z=xlim, zdir="x")
+    art3d.pathpatch_2d_to_3d(s, z=xlim, zdir="x")
     ax.set_aspect('auto')
     if(ylim == 0 or xlim == 0):
         xlim = 5
-        ax.set_zlim3d(0,5)
+        ax.set_zlim3d(0, 5)
 
     if xlim > ylim:
         ylim = xlim
     else:
         xlim = ylim
 
-    
-
     ax.set_xlim3d(0, xlim)
     ax.set_ylim3d(-ylim, ylim)
     plt.legend()
     plt.show()
-    plt.pause(.02)
+    plt.pause(.05)
+
 
 gatherData()
-
-
